46bcc1d8cee71c778ba1759458e6625739fa4f63
eberkenp <34282193+eberkenp@users.noreply.github.com>
2018-01-06 12:19:38 -0500

    Added simulation for newtons law of cooling

---
+++ simulate_exp3.m
@@ -0,0 +1,30 @@
+%Approximate (simulate) an exponential function by integrating the derivative
+%
+%function to approximate T(t)=Ta+(To-Ta)*exp^{-kt}  
+%
+%(dT}/{dt}=-k(T-Ta). 
+%T(0)=To  
+%
+%http://www.ugrad.math.ubc.ca/coursedoc/math100/notes/diffeqs/cool.html
+
+%Simulation
+dt = 0.01;
+t_n = [0:dt:5];
+R = 1;
+C = 1;
+Vb = 1;
+y0 = C*Vb*(1-exp(-0/(R*C)));
+index = 1;
+y_n = y0;
+while(t_n(index)<max(t_n));
+    dy(index) = (Vb*exp(-t_n(index)/(R*C))/R)*dt;
+    y_n(index+1) = y_n(index)+dy(index);
+    index = index + 1;
+end
+
+%Calculate true Exponential
+t = linspace(0,5,1000);
+y = C*Vb*(1-exp(-t/(R*C)));
+
+%Plot results
+plot(t, y, t_n, y_n, '.k');
