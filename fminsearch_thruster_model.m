92d1c70f3013d12aad22bab2b78ecdc78b3feaf2
eberkenpas <34271210+eberkenpas@users.noreply.github.com>
2018-01-08 07:02:02 -0500

    Added additional data test cases for fminsearch

--- fminsearch_thruster_model.m
+++ fminsearch_thruster_model.m
@@ -9,23 +9,40 @@ options.OutputFcn = [];
 options.PlotFcns = {[@optimplotfval],[ @optimplotx]};
 
 %Set up model params
-n = 200;
+n = 100;
 To = 0;
 Ta = ones(1,n);
 dt = 0.01;
 
-%Data to fit
-Ta_Data = 20;
-k_Data = 20;
-t_Data = linspace(0,20,10000);
-Data=Ta_Data+(To-Ta_Data)*exp(-k_Data*t_Data) + 0.1*randn(size(t_Data));
+%Data to fit (Newton's law of cooling)
+%Ta_Data = 20;
+%k_Data = 20;
+%t_Data = linspace(0,20,10000);
+%Data=Ta_Data+(To-Ta_Data)*exp(-k_Data*t_Data) + 0.1*randn(size(t_Data));
 
-t_fit = [0:dt:floor(max(t_Data)/dt)*dt];
+%Data to fit (Charge on a capacitor)
+%t = linspace(0,2,10000); %[s]
+%C = 1E-6; %[F]
+%R = 1E5; %[Ohms]
+%Vb = 1; %[V]
+%Q = C*Vb*(1-exp(-t/(R*C))) + 1E-8*randn(size(t));
+%t_Data = t;
+%Data = Q;
+
+%Data to fit (Second order system)
+t = linspace(0,2,10000); %[s]
+c1 = 1;
+r1 = 1;
+c2 = 1;
+r2 = -2;
+t_Data = t;
+Data = c1*exp(r1*t)+c2*exp(r2*t);
+
 
 %define Fitness Function
 f = @(x)fitness_fcn_thruster_curve(x, To, Ta, dt, t_Data, Data);
 
-[x fval exitflag opt_output] = fminsearch(f,[0 0], options)
+[x fval exitflag opt_output] = fminsearch(f,[2 0], options)
 
 %simulate and plot result
 k = x(1);
