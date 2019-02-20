5da736ddbf52946d40529a0ca5e58949a2f2ec32
eberkenp <eberkenp@ngs.org>
2019-02-20 15:24:04 -0500

    revised thrust_to_throttle_Demo.m for readability

--- thrust_to_throttle_Demo.m
+++ thrust_to_throttle_Demo.m
@@ -1,13 +1,13 @@
+Desired_Thrust = 0 %[N]
+
 Thrust_to_Throttle_Table = csvread('Thrust_to_Throttle_Table.csv');
-Thrust_to_Torque_Table = csvread('Thrust_to_Torque_Table.csv');
 Table_Thrusts = Thrust_to_Throttle_Table(:,1);
 Table_Throttles = Thrust_to_Throttle_Table(:,2);
-Table_Torques = Thrust_to_Torque_Table(:,2);
-
-Desired_Thrust = 0; %[N]
-
 Throttle = thrust_to_throttle(Desired_Thrust, Table_Thrusts, Table_Throttles) %[0-1]
 Throttle_Interp = interp1(Table_Thrusts, Table_Throttles, Desired_Thrust, 'linear') %[0-1]
 
+Thrust_to_Torque_Table = csvread('Thrust_to_Torque_Table.csv');
+Table_Thrusts = Thrust_to_Torque_Table(:,1);
+Table_Torques = Thrust_to_Torque_Table(:,2);
 Torque = thrust_to_torque(Desired_Thrust, Table_Thrusts, Table_Torques) %[Nm]
 Torque_Interp = interp1(Table_Thrusts, Table_Torques, Desired_Thrust, 'linear') %[Nm]
